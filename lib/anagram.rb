a1a75aa1bcffa31f29a00d44360ceee986556074
Alison Golden <alisong779@yahoo.com>
2019-04-24 01:03:24 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
- 
+class Anagram
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word
+  end 
+fdhsfh
+  def match(words)
+    words.select do |word|
+      word.split("").sort == @word.split("").sort  
+  end 
+end 
+end 
